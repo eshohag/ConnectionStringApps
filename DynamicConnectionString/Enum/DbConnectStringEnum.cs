934045753cd8ffb5f5228b3ae0bc8447dbfac35b
Shohag Mia <shohag.mia@datasoft-bd.com>
2019-06-23 11:55:23 +0600

    Add project files.

---
+++ DynamicConnectionString/Enum/DbConnectStringEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicConnectionString.Enum
+{
+    public enum DbConnectStringEnum
+    {
+        DevelopmentEnvironmentDb,
+        TestingEnvironmentDb,
+        ProductionEnvironmentDb,
+    }
+}
